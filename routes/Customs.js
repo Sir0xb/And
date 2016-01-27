e784e63046e04891f26ee5d7029f71dae408b701
Sir0xb <Sir0xb@live.com>
2016-01-27 18:58:20 +0800

    优化检验

--- routes/Customs.js
+++ routes/Customs.js
@@ -4,15 +4,9 @@ module.exports = function (app) {
     app.get("/:url", function (req, res, next) {
         Tools.log('Customs', 'get', req.params.url, '进入检查');
 
-        if (!req.session.user) {
+        if (!req.session.user && req.params.url != 'signup') {
             Tools.log('Customs', 'get', req.params.url, '未登录用户', 'login');
-            if (req.params.url == 'signup') {
-                Tools.pageJump(req, res, next, "index.html", "注册", "signup", {
-                    test: true
-                });
-            } else {
-                Tools.pageJump(req, res, next, "index.html", "登录", "login");
-            }
+            Tools.pageJump(req, res, next, "index.html", "登录", "login");
         } else {
             Tools.log('Customs', 'get', req.params.url, '登陆用户，放行');
             next();
