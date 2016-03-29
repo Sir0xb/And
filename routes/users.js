2e1b6625ff509bdda6d7e2489dbdf9545f9f71ea
Sir0xb <Sir0xb@live.com>
2016-03-29 16:19:25 +0800

    ...

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var Tools = require("./resTools");
-
-module.exports = function(app) {
-    app.get("/users", function (req, res, next) {
-        Tools.log('Users', 'get', 'users', '用户管理页面', 'users');
-        Tools.pageJump(req, res, next, "index.html", "用户管理", "users");
-    });
-};
